fce0ff2898f15551efb9c63e450be6fac291bf3d
Aliyu Yusuf <usoofali@gmail.com>
2026-06-16 11:12:19 +0100

    Blah blah

--- resources/views/emails/driver-logistics-booking.blade.php
+++ resources/views/emails/driver-logistics-booking.blade.php
@@ -1,35 +1,39 @@
 <x-mail::message>
-@if (! empty($emailLogo))
-<p style="text-align:center; margin-bottom: 24px;">
-    <img src="{{ $emailLogo }}" alt="{{ $companyName }}" style="max-height:80px; width:auto;">
-</p>
-@endif
+    @if (!empty($emailLogo))
+        <p style="text-align:center; margin-bottom: 24px;">
+            <img src="{{ $emailLogo }}" alt="{{ $companyName }}" style="max-height:80px; width:auto;">
+        </p>
+    @endif
 
-# {{ __('New Dock Receipt Available') }}
+    # {{ __('New Dock Receipt Available') }}
 
-{{ __('Hello!') }}
+    {{ __('Hello!') }}
 
-{{ __('A new Dock Receipt has been generated for shipment :ref.', [
+    {{ __('A new Dock Receipt has been generated for shipment :ref.', [
     'ref' => $shipment->reference_no,
 ]) }}
 
-{{ __('You can download the document using the button below. This link will expire in 7 days.') }}
+    {{ __('You can download the document using the button below. This link will expire in 7 days.') }}
 
-<x-mail::button :url="$signedUrl">
-{{ __('Download Dock Receipt') }}
-</x-mail::button>
+    <x-mail::button :url="$signedUrl">
+        {{ __('Download Dock Receipt') }}
+    </x-mail::button>
 
-**{{ __('Shipment Details') }}**
-- **{{ __('Reference') }}:** {{ $shipment->reference_no }}
+    <x-mail::button :url="$setting->getWhatsAppUrl('Hi! I have a question about the logistics for shipment ' . $shipment->reference_no)" color="success">
+        {{ __('Chat on WhatsApp') }}
+    </x-mail::button>
 
----
-Thanks,
-**{{ $companyName }}**
+    **{{ __('Shipment Details') }}**
+    - **{{ __('Reference') }}:** {{ $shipment->reference_no }}
 
-@if (! empty($setting->address) || ! empty($setting->phone) || ! empty($location))
-<p style="color: #718096; font-size: 0.75rem; line-height: 1.25rem;">
-    {{ $setting->address }} {{ $location }}<br>
-    {{ $setting->phone }}
-</p>
-@endif
+    ---
+    Thanks,
+    **{{ $companyName }}**
+
+    @if (!empty($setting->address) || !empty($setting->phone) || !empty($location))
+        <p style="color: #718096; font-size: 0.75rem; line-height: 1.25rem;">
+            {{ $setting->address }} {{ $location }}<br>
+            {{ $setting->phone }}
+        </p>
+    @endif
 </x-mail::message>
